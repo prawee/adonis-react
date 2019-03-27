36a4a0637810184f2907bf5568b1565351438a76
Prawee Wongsa <prawee.w@integra8t.com>
2019-03-27 14:58:01 +0700

    make user controller

---
+++ app/Controllers/Http/UserController.js
@@ -0,0 +1,44 @@
+/*
+ * @Author: Prawee Wongsa prawee.w@integra8t.com 
+ * @Date: 2019-03-27 14:38:01 
+ * @Last Modified by: Prawee Wongsa
+ * @Last Modified time: 2019-03-27 14:57:25
+ */
+
+'use strict'
+
+const User = use('App/Models/User')
+
+class UserController {
+    async login({ request, response, auth }) {
+        const { email, password } = request.only(['email', 'password'])
+        const token = await auth.attempt(email, password)
+        return response.json(token)
+    }
+
+    async register({ request, response }) {
+        const { first_name, last_name, email, password } = request.only([
+            'first_name', 'last_name',
+            'email', 'password'
+        ])
+        await User.create({
+            first_name,
+            last_name,
+            email,
+            password
+        })
+        return response.send({ message: 'User has been created'})
+    }
+
+    async show({ params, response }) {
+        const user = await User.find(params.id)
+        const res = {
+            first_name: user.first_name,
+            last_name: user.last_name,
+            email: user.email
+        }
+        return response.json(res)
+    }
+}
+
+module.exports = UserController
